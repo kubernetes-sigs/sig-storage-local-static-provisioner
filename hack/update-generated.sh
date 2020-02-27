62e664124b96ac46957bb5da108c74c071bc58d1
Hendrik Ferber <blackfon83@googlemail.com>
2020-02-27 18:43:16 +0100

    added release-name and namespace to template generator script

--- hack/update-generated.sh
+++ hack/update-generated.sh
@@ -51,8 +51,13 @@ FILES=$(ls examples/)
 for f in $FILES; do
     input="examples/$f"
     generated="generated_examples/$f"
+<<<<<<< HEAD
     printf "Generating with helm v${HELM_VERSION} %s from %s\n" $generated $input
     $HELM_BIN template --dry-run -f examples/$f local-static-provisioner --namespace default ./provisioner > $generated
+=======
+    printf "Generating %s from %s\n" $generated $input
+    $HELM_BIN template -f examples/$f  --name local-static-provisioner --namespace default ./provisioner > generated_examples/$f
+>>>>>>> 9a269d5... added release-name and namespace to template generator script
 done
 
 echo "Done."
